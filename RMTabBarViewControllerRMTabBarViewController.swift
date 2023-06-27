4273ea5464640347fb7653eb4b4e9217097caaed
Sumayyah <99235959+Semsemq@users.noreply.github.com>
2023-06-27 06:53:39 +0300

    Add files via upload

---
+++ RMTabBarViewControllerRMTabBarViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  الجوكر
+//
+//  Created by Sumayyah AHmad on 08/12/1444 AH.
+//
+
+import UIKit
+
+class RMTabBarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       print("ViewDidload")
+        view.backgroundColor = .darkGray
+    }
+
+
+}
+
